84164ccea8edc5e9cf2c888bc94e8ba626fb65b6
sadMaxim <dresvyannikovm@gmail.com>
2023-10-19 17:36:32 +0000

    added purescript in main flake

--- flake.nix
+++ flake.nix
@@ -11,9 +11,19 @@
   inputs.hackage.follows = "plutus/hackage";
   inputs.CHaP.follows = "plutus/CHaP";
   inputs.haskell-nix.follows = "plutus/haskell-nix";
+  inputs.flake-compat = {
+    url = "github:edolstra/flake-compat";
+    flake = false;
+  };
+  inputs.ctl = {
+    type = "github";
+    owner = "Plutonomicon";
+    repo = "cardano-transaction-lib";
+    rev = "605931759ff35bdd71bb4d933071aced9fb57870";
+  };
 
 
-  outputs = { self, nixpkgs, flake-utils, cardano, iohk-nix, ... }@inputs:
+  outputs = { self, nixpkgs, flake-utils, cardano, iohk-nix, ctl, ... }@inputs:
     let
       onchain-outputs = inputs.iogx.lib.mkFlake {
         inherit inputs;
@@ -25,15 +35,40 @@
       (system:
         let
           overlays = [
+            ctl.overlays.purescript
+            ctl.overlays.runtime
+            ctl.overlays.spago
           ];
           pkgs = import nixpkgs {
             inherit system overlays; config.permittedInsecurePackages =
             [
               "nodejs-14.21.3"
-              "openssl-1.1.1w"
+              "openssl-1.1.1t"
             ];
             config.allowBroken = true;
           };
+          psProjectFor = pkgs:
+            pkgs.purescriptProject rec {
+              inherit pkgs;
+              projectName = "ctl-scaffold";
+              packageJson = ./spamer/basic-spamer-ctl/package.json;
+              packageLock = ./spamer/basic-spamer-ctl/package-lock.json;
+              src = builtins.path {
+                path = ./spamer/basic-spamer-ctl;
+                name = "${projectName}-src";
+                # Adjust the `filter` as necessary
+                filter = path: ftype: !(pkgs.lib.hasSuffix ".md" path);
+              };
+              shell = {
+                withRuntime = true;
+                packageLockOnly = true;
+                packages = with pkgs; [
+                  fd
+                  nodePackages.eslint
+                  nodePackages.prettier
+                ];
+              };
+            };
 
           devShell = with pkgs; mkShell {
             buildInputs = [
@@ -42,8 +77,8 @@
               cardano.legacyPackages.${system}.cardano-node
               postgresql_14
             ] ++ (with pkgs.python310Packages; [ jupyterlab pandas psycopg2 ])
-            ++ onchain-outputs.devShell.${system}.buildInputs;
-            # ++ spamer-ctl.outputs.devShells.${system}.default.buildInputs
+            ++ onchain-outputs.devShell.${system}.buildInputs
+            ++ (psProjectFor pkgs).devShell.buildInputs;
             shellHook = ''
             '';
           };
