f2ab50d9d6913b4145c01fb52f6d044d1053fd99
sadMaxim <dresvyannikovm@gmail.com>
2023-11-21 13:53:27 +0000

    fix ctl update

--- spammer/spammer/src/Main.purs
+++ spammer/spammer/src/Main.purs
@@ -116,6 +116,7 @@ s1 = "4e4d01000033222220051200120011"
 s2 = "480100002221200101"
 main :: Effect Unit
 main = do 
+  log "hi"
   -- arr <- liftMaybe (error "no parse") (decodeCborHexToBytes s2)
   -- let script = plutusV2Script arr
   -- log $ show $ script
