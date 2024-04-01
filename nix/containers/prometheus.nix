29e792f7b254c605224d0cd2fe205bb861636ca8
sadMaxim <dresvyannikovm@gmail.com>
2024-04-01 22:09:24 +0000

    change prometheus storage period

--- nix/containers/prometheus.nix
+++ nix/containers/prometheus.nix
@@ -22,7 +22,7 @@
       text = ''
         ln -sf ${pkgs.iana-etc}/etc/protocols /etc/protocols
         ln -sf ${pkgs.iana-etc}/etc/services /etc/services
-        ${pkgs.prometheus}/bin/prometheus --config.file=${./prometheus.yaml} --storage.tsdb.path=/data --storage.tsdb.retention.time=6m
+        ${pkgs.prometheus}/bin/prometheus --config.file=${./prometheus.yaml} --storage.tsdb.path=/data --storage.tsdb.retention.time=4320h
       '';
     };
   };
