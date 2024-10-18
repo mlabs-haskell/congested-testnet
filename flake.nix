5c21eca961467e049223f19d180faf95e9761ffb
sadMaxim <dresvyannikovm@gmail.com>
2024-10-18 15:22:29 +0000

    update ctl

--- flake.nix
+++ flake.nix
@@ -5,8 +5,7 @@
   inputs.aiken.url = "github:aiken-lang/aiken/7b452c21f01e5342b2c210e0a32f024c58ea2693";
   inputs.easy-purescript-nix.url = "github:justinwoo/easy-purescript-nix";
   inputs.ctl.url = "github:Plutonomicon/cardano-transaction-lib/v9.3.1";
-  inputs.ctl-old.url = "github:Plutonomicon/cardano-transaction-lib/b212a58a544d979b5e49dfe5db7f623a2c69e25b";
-  inputs.nixpkgs.follows = "ctl-old/nixpkgs";
+  inputs.nixpkgs.follows = "ctl/nixpkgs";
   inputs.flake-compat = {
     url = "github:edolstra/flake-compat";
     flake = false;
