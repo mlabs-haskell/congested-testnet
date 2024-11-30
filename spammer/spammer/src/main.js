a88146a43d595ce60c25abe809e57cad00b5c7c3
sadMaxim <dresvyannikovm@gmail.com>
2024-11-30 22:39:38 +0000

    nspammers 4

--- spammer/spammer/src/main.js
+++ spammer/spammer/src/main.js
@@ -1,7 +1,7 @@
 (async () => {
   const {Worker, workerData} = await import("node:worker_threads");
   const path = await import("node:path");
-  const nSpammers = 5 
+  const nSpammers = 4 
   const spammers = Array.from({length : nSpammers},(_,idSpammer) => new Worker(path.resolve(__dirname, "./spammer.js"), {workerData: idSpammer})) 
   // activate wallets in each spammer one by one 
   spammers[0].postMessage("unpause")
